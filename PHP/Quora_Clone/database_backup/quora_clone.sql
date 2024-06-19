bcf4a0e076d0151950eedad6bfe0011a5a1c89ac
ShubhamPaliwal03 <shubhampaliwal3032003@gmail.com>
2024-06-19 20:27:56 +0530

    added files

---
+++ PHP/Quora_Clone/database_backup/quora_clone.sql
@@ -0,0 +1,108 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 19, 2024 at 04:56 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `quora_clone`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `pid` int(11) NOT NULL,
+  `uid` int(11) NOT NULL,
+  `post_content` varchar(1000) NOT NULL,
+  `date_and_time` datetime NOT NULL,
+  `post_image` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`pid`, `uid`, `post_content`, `date_and_time`, `post_image`) VALUES
+(1, 1, 'This is the first post from this Quora clone.', '2024-06-19 16:45:48', 'none'),
+(2, 1, 'This is the first post from this Quora clone.', '2024-06-19 16:54:30', 'none'),
+(3, 1, 'Testing...', '2024-06-19 16:54:44', 'none'),
+(4, 1, 'Again...', '2024-06-19 16:55:05', 'none'),
+(5, 1, 'Again...', '2024-06-19 16:55:42', 'none');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `uid` int(11) NOT NULL,
+  `username` varchar(25) DEFAULT NULL,
+  `password` varchar(25) NOT NULL,
+  `security_question` varchar(100) NOT NULL,
+  `security_answer` varchar(100) NOT NULL,
+  `user_image` varchar(25) DEFAULT NULL,
+  `user_title` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`uid`, `username`, `password`, `security_question`, `security_answer`, `user_image`, `user_title`) VALUES
+(1, 'Shubham Paliwal', 'sp@12345', 'What is the name of the city in which you were born?', 'Udaipur', NULL, NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`pid`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`uid`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
